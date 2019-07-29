4e90adfce12794f7a5d8835c6049dd6f3832e6a5
delate12137 <52997010+delate12137@users.noreply.github.com>
2019-07-29 16:12:02 +0800

    Add files via upload

---
+++ oled/OLED12864.c
@@ -0,0 +1,21 @@
+#include "intrins.h"
+#include "codetab.h"
+#include "oled.h"
+
+void main(void)
+{
+	unsigned char i;
+
+	OLED_Init(); //OLED初始化
+	for(;;)					//单页面  while 会一直刷  for不会
+	{
+		OLED_Fill(0xff); //屏全亮
+		delay(2000);
+		OLED_Fill(0x00); //屏全灭
+		delay(200);
+
+		Draw_BMP(0,0,128,8,BMP2);								
+		delay(8000);
+//			OLED_CLS();//清屏
+	}
+}
